acd1512482c143babde3ae86604cc9d5cd9fe2bf
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-09-12 13:49:14 +0530

    second commit

--- app.js
+++ app.js
@@ -101,6 +101,8 @@ app.post("/delete",function(req,res){
   res.redirect("/");
 
 });
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
